a775a55a7cd3c2572b334852a67e21b2af51e9f8
krijn <grimmekrijn@gmail.com>
2022-11-19 23:49:12 +0100

    Poging 1

---
+++ DatabaseTest/Gerechten.cs
@@ -0,0 +1,22 @@
+using MySql.Data.MySqlClient;
+
+namespace DatabaseTest;
+
+public class Gerechten
+{
+    public string Naam { get; set; }
+    public string Beschrijving { get; set; }
+    public int Prijs { get; set; }
+
+    public Gerechten(string naam, string beschrijving, int prijs)
+    {
+        Naam = naam;
+        Beschrijving = beschrijving;
+        Prijs = prijs;
+    }
+
+    public override string ToString()
+    {
+        return $"Naam: {Naam}, Beschrijving: {Beschrijving}, Prijs: {Prijs}";
+    }
+}
---
+++ DatabaseTest/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Data;
+using System.Reflection;
+using DatabaseTest;
+using MySql.Data.MySqlClient;
+
+
+var props = typeof(Gerechten).GetProperties();
+
+// join props togehter with a comma
+var propsJoined = string.Join(",", props.Select(p => p.Name));
+
+MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+builder.Server = "localhost";
+builder.UserID = "root";
+builder.Database = "maccie";
+MySqlConnection connection = new MySqlConnection(builder.ToString());
+connection.Open();
+MySqlCommand command = connection.CreateCommand();
+command.CommandText = $"SELECT {propsJoined} FROM `gerechten`";
+
+MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+
+DataSet ds = new DataSet();
+adapter.Fill(ds);
+
+List<Gerechten> gerechten = new List<Gerechten>();
+
+foreach (DataRow row in ds.Tables[0].Rows)
+{
+    gerechten.Add(new Gerechten((string) row["Naam"], (string) row["Beschrijving"], (int) row["Prijs"]));
+}
+
+connection.Close();
+
+foreach (var gerecht in gerechten)
+{
+    Console.WriteLine(gerecht);
+}
