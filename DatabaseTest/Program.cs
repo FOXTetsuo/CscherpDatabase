97a53b981bb5791e1de5e946241ad7dfb4429dac
krijn <grimmekrijn@gmail.com>
2022-11-20 01:00:43 +0100

    Cleanup

--- DatabaseTest/Program.cs
+++ DatabaseTest/Program.cs
@@ -2,19 +2,8 @@
 
 using DatabaseTest;
 
-Database.Database.Connect();
-
 DataSet dataSet = new DataSet();
 
 dataSet.Fill();
 
 dataSet.PrintLists();
-
-// List<Gerechten> gerechten = Database.Database.GenerateList<Gerechten>();
-
-// foreach (Gerechten gerecht in gerechten)
-// {
-    // Console.WriteLine(gerecht);
-// }
-
-Database.Database.Disconnect();
